73295b3eae7fde3c973dcae0d20713d5a854f62a
Julian <julianrosenthal@yahoo.com>
2021-08-09 11:54:47 -0700

    moved some code into parent component

--- src/components/Sudoku/Board.js
+++ src/components/Sudoku/Board.js
@@ -1,33 +1,23 @@
-import React, { useState } from 'react';
 import Cell from './Cell';
+
 const ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-
 const Board = (props) => {
-  const [cellValues, setCellValues] = useState(props.cellValues)
-
-  const handleCellChange = (event) => {
-    const target = event.target
-    const value = Number(target.value)
-    const parent = target.parentElement
-    const id = parent.id.split('-')[1] // ids are in form c-A0 to c-I8
-    const row = id[0]
-    const col = Number(id[1])
-    const aIDX = ALPHABET.indexOf(row) * 9 + col // 00 to 80
-    setCellValues([...cellValues.slice(0, aIDX), value, ...cellValues.slice(aIDX + 1)])
-    
+  
+  const genCellID = (i) => {
+    let cellNum = ALPHABET[Math.floor(i / 9)] + i % 9
+    return `c-${cellNum}`
   }
 
   const renderCells = (values) => {
     return values.map(
       (v, i) => {
-        let cellNum = ALPHABET[Math.floor(i / 9)] + i % 9
-        let id = `c-${cellNum}`
+        const id = genCellID(i)
         return (
           <Cell 
             key={i} 
             id={id} 
             value={v}
-            handleChange={handleCellChange}
+            handleChange={props.handleCellChange}
           />
         )
       }
@@ -35,21 +25,11 @@ const Board = (props) => {
     )
   }
 
-  console.log(cellValues)
   return (
     <div id="board">
-      {renderCells(cellValues)}
+      {renderCells(props.cellValues)}
     </div>
   )
-}
-
-
-const genDefaultValues = () => {
-  return Array.from({length: 81}, () => 0);
-}
-
-Board.defaultProps = {
-  cellValues: genDefaultValues()
 }
 
 export default Board

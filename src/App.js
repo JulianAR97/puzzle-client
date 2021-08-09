61a8a9a612ccf3dbabe2a5971dbdfc2c5d3d55f4
Julian <julianrosenthal@yahoo.com>
2021-08-09 10:23:33 -0700

    app renders sudoku

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-import Navbar from './components/Navbar.js'
+import Navbar from './components/Nav/Navbar.js'
+import Sudoku from './components/Sudoku/Sudoku.js'
 
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
+      <Sudoku></Sudoku>
     </div>
   );
 }

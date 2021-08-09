0b31eb54d972690d5f1d06a41740f180f93de7e0
Julian <julianrosenthal@yahoo.com>
2021-08-09 11:43:58 -0700

    added Helpers file

---
+++ src/Helpers.js
@@ -0,0 +1,13 @@
+
+/**
+ * Takes length of array and value and returns an array filled with that value
+ * @example
+ * genArray(5, 1) => [1, 1, 1, 1, 1]
+ * @param {Number} len 
+ * @param {*} val 
+ * @returns {Array} array
+ */
+export const genArray = (len, val) => {
+  return Array.from({length: len}, () => val);
+}
+

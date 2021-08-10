9f16206b8c6fd437ad17477451fa4c6d8d2910f1
Julian <julianrosenthal@yahoo.com>
2021-08-10 10:11:34 -0700

    moved margins to about (parent)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Sudoku from './components/Sudoku/Sudoku.js'
+import About from './components/About/About.js'
 
 function App() {
   return (
     <div className="App">
       <Sudoku></Sudoku>
+      <About></About>
     </div>
   );
 }

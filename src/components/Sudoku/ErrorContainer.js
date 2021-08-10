a8d4a17e89c8b07b2ab60126a6870f8010f2a6e4
Julian <julianrosenthal@yahoo.com>
2021-08-10 13:21:21 -0700

    added ErrorContainer component

---
+++ src/components/Sudoku/ErrorContainer.js
@@ -0,0 +1,22 @@
+import './ErrorContainer.css'
+
+const ErrorContainer = (props) => {
+  const renderErrors = (errors) => {
+    return errors.map(
+      (error, i) => 
+        <p key={i}>{error}</p>
+    )
+  }
+  return (
+    <div id="errorContainer">
+      {renderErrors(props.errors)}
+    </div>
+  )
+}
+
+ErrorContainer.defaultProps = {
+  errors: []
+}
+
+export default ErrorContainer
+

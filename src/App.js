d9588d09aba67417e3e4dbd7410bbf4829788bfc
Julian <julianrosenthal@yahoo.com>
2021-08-10 11:19:25 -0700

    fetches markdown upon mount

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import './App.css';
+import React, {useEffect, useState} from 'react';
 import Sudoku from './components/Sudoku/Sudoku.js'
 import About from './components/About/About.js'
+import './App.css';
 
 function App() {
+  const aboutFP = "./components/About/About.md"
+  const [aboutMD, setAboutMD] = useState('')
+
+  useEffect(() => {
+    import (`${aboutFP}`) // unclear why it can't find file without interpolation here
+      .then(res => {
+        fetch(res.default) // .default is path to file
+          .then(res => res.text())
+          .then(res => setAboutMD(res))
+      })
+      .catch(err => console.error(`error: `, err))
+  })
+
   return (
     <div className="App">
-      <Sudoku></Sudoku>
-      <About></About>
+      <Sudoku />
+      <About content={aboutMD} />
     </div>
   );
 }
